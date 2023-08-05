bccef4b7d18d2a2e6ddb126272f64713296e05b1
RasulPirsoltanov <7m0uvuf@code.edu.az>
2023-08-05 13:44:37 +0400

    first commit

---
+++ TravelGoApi/Controllers/VisitorController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TravelGoApi.DAL.Context;
+using TravelGoApi.DAL.Entities;
+
+namespace TravelGoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VisitorController : ControllerBase
+    {
+        private readonly AppDbContext _context = new AppDbContext();
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            return Ok(await _context.Visitors.ToListAsync());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(Visitor visitor)
+        {
+            await _context.Visitors.AddAsync(visitor);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            Visitor visitor = await _context.Visitors.FindAsync(id);
+            if(visitor== null)
+            {
+                return NotFound();
+            }
+            return Ok(visitor);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Visitor? visitor = await _context.Visitors.FindAsync(id);
+            if (visitor == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(visitor);
+            await _context.SaveChangesAsync();
+            return Ok(visitor);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(Visitor visitor)
+        {
+            _context.Visitors.Update(visitor);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ TravelGoApi/DAL/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelGoApi.DAL.Entities;
+
+namespace TravelGoApi.DAL.Context
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-UGCLLOE\\MSSQLSERVER2;Database=TravelGoApiDB;integrated security=true;Encrypt=false;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Visitor> Visitors { get; set; }
+
+    }
+}
